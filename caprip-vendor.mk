86e7cf5b5026651d8fb1135d52ca997ae45e5397
Deivid21 <david.parra.ignacio@gmail.com>
2026-09-03 00:47:44 -0400

    caprip: Decommonize perf blobs
    Performance configurations contain device-specific CPU frequencies,
    power hints and target resources.
    
    Change-Id: If633443c2ba64538db3b9bb3649214daf53ca306

--- caprip-vendor.mk
+++ caprip-vendor.mk
@@ -2772,8 +2772,16 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/caprip/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-fpc2.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service-fpc2.rc \
     vendor/motorola/caprip/proprietary/vendor/etc/init/android.hardware.nfc@1.2-service-moto.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.nfc@1.2-service-moto.rc \
     vendor/motorola/caprip/proprietary/vendor/etc/init/init_thermal-engine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init_thermal-engine.rc \
+    vendor/motorola/caprip/proprietary/vendor/etc/init/vendor.qti.hardware.perf@2.2-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf@2.2-service.rc \
     vendor/motorola/caprip/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     vendor/motorola/caprip/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/commonsysnodesconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonsysnodesconfigs.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
+    vendor/motorola/caprip/proprietary/vendor/etc/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     vendor/motorola/caprip/proprietary/vendor/etc/qdcm_calib_data_mipi_mot_vid_djn_nt36525c_652.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_mot_vid_djn_nt36525c_652.xml \
     vendor/motorola/caprip/proprietary/vendor/etc/qdcm_calib_data_mipi_mot_vid_tianma_ili9882n_652.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_mot_vid_tianma_ili9882n_652.xml \
     vendor/motorola/caprip/proprietary/vendor/etc/qdcm_calib_data_mipi_mot_vid_txd_ili9882n_652.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_mot_vid_txd_ili9882n_652.xml \
@@ -2866,6 +2874,12 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/caprip/proprietary/vendor/usr/keylayout/uinput-fpsensor.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpsensor.kl
 
 PRODUCT_PACKAGES += \
+    libperfconfig \
+    libperfgluelayer \
+    libperfioctl \
+    libqti-perfd-client \
+    libqti-perfd \
+    libqti-util \
     libthermalclient \
     com.mot.eeprom.mot_gt24p64e_ov8856_eeprom \
     com.mot.eeprom.mot_gt24p64e_s5k3l6_eeprom \
@@ -3001,4 +3015,5 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service-chipone \
     android.hardware.biometrics.fingerprint@2.1-service-fpc \
     fpc_ident \
+    vendor.qti.hardware.perf@2.2-service \
     thermal-engine
